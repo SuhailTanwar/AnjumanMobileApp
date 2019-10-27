adb715316c65745258fce2d6156f81b58a25246e
SuhailTanwar <tanwarsohail2010@gmail.com>
2019-10-27 21:13:34 +0530

    UI Chnages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' }, 
   { path: 'create-account', loadChildren: './create-account/create-account.module#CreateAccountPageModule' },
+  
+
 
 ];
 
